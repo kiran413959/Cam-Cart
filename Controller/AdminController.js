7133d9256b8497cb5331e4db992a80cbbaa657ee
Kiran413959 <kiranrajendran6@gmail.com>
2024-04-09 13:16:13 +0530

    completed

--- Controller/AdminController.js
+++ Controller/AdminController.js
@@ -187,7 +187,16 @@ module.exports = {
 
 
     logoutget: (req, res) => {
-        res.redirect('/admin_login')
+
+        req.session.destroy((err) => {
+            if (!err) {
+              console.error("Error destroying session:", err);
+              return res.redirect('/admin_login')
+
+            }
+        
+          });
+ 
 
 
 
